2c5f19a97e2c89a6ca4082f534903d574a737a95
Hector Miranda <marcetux@gmail.com>
2017-02-10 19:06:24 -0800

    Marking all tests as not implemented

--- spec/component_manager_spec.rb
+++ spec/component_manager_spec.rb
@@ -9,24 +9,28 @@ describe ComponentManager do
     describe 'add_dependency' do
         it 'Adds a component name and its dependencies to the depency table' do
             # expect(componentManager.dependencyTable).to include(@dependency)
+            raise 'not implemented'
         end
     end
 
     describe 'install_component' do
         it 'Adds a component object to the installed_components hash' do
             # expect(componentManager.installed_components).to include(@component)
+            raise 'not implemented'
         end
     end
 
     describe 'remove_component' do
         it 'Verifies that dependencies are not being used by other components and if so remove the component' do
             # expect(componentManager.installed_components).to not include(@dependency)
+            raise 'not implemented'
         end
     end
 
     describe 'list_installed_components' do
         it 'Enumerate the content of the installed_components array' do
             # expect(componentManager.dependencyTable).to include(@dependency)
+            raise 'not implemented'
         end
     end
 end
